b00988004631dfc975f49b6e1e362bc8b6d5986c
Ehab Mansour <81818344+ehabmansour1@users.noreply.github.com>
2023-06-26 22:03:20 +0300

    Add files via upload

---
+++ javascript/main.js
@@ -0,0 +1,16 @@
+//menu ================================
+let menuButt = document.querySelector("header .menu");
+let exitButt = document.querySelector("header .exit");
+let nav = document.querySelector("header nav");
+menuButt.addEventListener("click", () => {
+  nav.style.right = "0";
+});
+exitButt.addEventListener("click", () => {
+  nav.style.right = "-232px";
+});
+let links = document.querySelectorAll("header .links a");
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    nav.style.right = "-232px";
+  });
+});
